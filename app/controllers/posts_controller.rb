066447669c1e9822a8c770c68f694d0d13e45886
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-20 17:43:31 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PostsController < ApplicationController
 	def index
 		@posts = Post.all
@@ -12,6 +13,7 @@ class PostsController < ApplicationController
 	  post.title = params[:title]
 	  post.description = params[:description]
 	  post.save
+		binding.pry
 	  redirect_to post_path(@post)
 	end
 
